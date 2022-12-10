258f22ee0008594e9e0ef1701a174cf9dfcdb55e
Edison Araujo <120268608+Edracoder@users.noreply.github.com>
2022-12-10 16:40:00 -0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,17 @@
+# The list [] serves do show a set of options, being 0 the initial point.
+#リスト [] サーブは一連のオプションを表示します。0 が初期ポイントです。
+
+cdz = ['seiya', 'hyoga', 'shun', 'shiryuu', 'ikki']
+print(cdz[0])
+
+cdz = ['seiya', 'hyoga', 'shun', 'shiryuu', 'ikki']
+print(cdz[0].title())
+
+print("my favorite knight of Zodiac was " + cdz[0].title() +".")
+
+cars_list=['honda','toyota','mitsubishi']
+print(cars_list)
+
+cars_list[0]= "hyundai"
+print(cars_list)
+
